1cad553c19d408c752e777a457c90cc65853a29b
kamal kashyap <kamal.kashyap13@gmail.com>
2018-05-03 03:46:22 +0530

    url

--- questions/urls.py
+++ questions/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # path('add-question', views.question_add, name='question_add'),
-    path('accounts/profile', views.profile, name='profile'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('questions/add-question', views.question_add, name='question_add'),
     path('questions/', views.index, name='index'),
-    path('add-question', views.question_add, name='question_add'),
+
 ]
